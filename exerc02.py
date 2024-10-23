c4e6e7576d2a122cb84bfa912b6ecb4e87712754
Gustavo <gussouza0088@gmail.com>
2024-10-23 18:41:08 -0300

    exercicio para verificar resto da divisão

---
+++ exerc02.py
@@ -0,0 +1,6 @@
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+valor1 = int(input("Digite o número a ser dividido: "))
+valor_div = valor1 % 5
+
+print("Resto da divisão: ", valor_div)
