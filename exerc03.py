b85129a6d556f6faac58cf2a3b734e13f5bbeec8
Gustavo <gussouza0088@gmail.com>
2024-10-23 18:46:31 -0300

    exercicio para multiplicação de 2 inputs

---
+++ exerc03.py
@@ -0,0 +1,6 @@
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+valor1 = int(input("Digite o primeiro valor a ser multiplicado: "))
+valor2 = int(input("Digite o segundo valor a ser multiplicado: "))
+valores_mult = valor1 * valor2 
+
+print("Multiplicação dos números inteiros inseridos: ", valores_mult)
