d40455145af729fea308b4c4ad26f63f9bdc9596
Gustavo <gussouza0088@gmail.com>
2024-10-23 18:26:26 -0300

    exercicio para soma de numeros inteiros

---
+++ exerc01.py
@@ -0,0 +1,7 @@
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+valor1 = int(input("Digite o primeiro valor a ser somado: "))
+valor2 = int(input("Digite o segundo valor a ser somado: "))
+valores_somados = valor1 + valor2 
+
+print("Soma dos números inteiros inseridos: ", valores_somados)
+print("end")
